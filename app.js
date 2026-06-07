40a8b2699d828e57bfde70042c15821be8e01655
sahildubey11 <sahildubey2004fb@gmail.com>
2026-06-07 18:20:08 +0530

    first

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Jenkins CI/CD Working Successfully!');
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
+
